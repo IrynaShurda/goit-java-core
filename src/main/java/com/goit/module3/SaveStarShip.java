4570298e698cf7bd7d2bb99b974b437e6b0569a7
i.shurda <9594900@gmail.com>
2022-11-17 19:51:13 +0200

    Module3: tasks 16 IF/ELSE

--- src/main/java/com/goit/module3/SaveStarShip.java
+++ src/main/java/com/goit/module3/SaveStarShip.java
@@ -1,18 +1,43 @@
 package com.goit.module3;
 
+import java.util.Arrays;
+
 public class SaveStarShip {
     //Test output
     public static void main(String[] args) {
         SaveStarShip ship = new SaveStarShip();
 
-        //Should be 10
-        System.out.println(ship.calculateDistance(-10));
+        //Завдання 15
+        //System.out.println(ship.calculateDistance(-10));
+
+        //Завдання 16
+        System.out.println(Arrays.toString(ship.getPlanets
+                ("DangerBanger")));
+
     }
-    public int calculateDistance(int distance){
+
+    //Завдання 15
+    /*public int calculateDistance(int distance){
         if (distance > 0) {
             return distance;
         } else {
             return Math.abs(distance);
+        }
+    }*/
+    //Завдання 16
+    public String[] getPlanets(String galaxy) {
+        String[] miaru = {"Maux", "Reux", "Piax"};
+        String[] milkyway = {"Earth", "Mars", "Jupiter"};
+        String[] dangerBanger = {"Fobius", "Demius"};
+        String[] empty = {};
+        if (galaxy.equals("Miaru")) {
+            return miaru;
+        } else if (galaxy.equals("Milkyway")) {
+            return milkyway;
+        } else if (galaxy.equals("DangerBanger")) {
+            return dangerBanger;
+        } else {
+            return empty;
         }
     }
 }
