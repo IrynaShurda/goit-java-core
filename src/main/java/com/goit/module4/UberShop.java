422956ce0668a56d6d1a85d798b3f03271fac7ad
i.shurda <9594900@gmail.com>
2022-11-24 15:14:56 +0200

    Module4: tasks 2 for

--- src/main/java/com/goit/module4/UberShop.java
+++ src/main/java/com/goit/module4/UberShop.java
@@ -8,10 +8,16 @@ public class UberShop {
         UberShop shop = new UberShop();
 
         //Should be 100 jup. 23.5 jup. 400 jup. - one price per line
-        float[] prices = new float[] {100f, 23.5f, 400f};
-        shop.printPrices(prices);
+        /*float[] prices = new float[] {100f, 23.5f, 400f};
+        shop.printPrices(prices);*/
+
+        //Should be [200, 2250]
+        float[] prices = new float[]{100f, 1500f};
+        shop.multiplyPrices(prices);
+        System.out.println(Arrays.toString(prices));
     }
-    public void printPrices(float[] prices) {
+
+    /*public void printPrices(float[] prices) {
         for (float price : prices) {
             System.out.println(price + " jup." );
         }
@@ -28,6 +34,17 @@ public class UberShop {
 
        for (int i=0;i<prices.length;++i){
             System.out.println(prices[i]+ " jup." );
+        }*/
+    public void multiplyPrices(float[] prices) {
+        for (int i = 0; i < prices.length; i++) {
+            //float newPrices;
+            if (prices[i] < 1000f) {
+                //System.out.println(prices[i] * 2);
+                prices[i]=prices[i] * 2;
+            } else {
+                //System.out.println(prices[i] * 1.5);
+                prices[i]=prices[i] * 1.5f;
+            }
         }
     }
 }
