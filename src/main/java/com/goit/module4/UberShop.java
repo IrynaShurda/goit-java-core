df9f396cc1fd757513b8d57eb5e44da7bb69e513
i.shurda <9594900@gmail.com>
2022-11-27 18:44:49 +0200

    Module4: tasks 3 array

--- src/main/java/com/goit/module4/UberShop.java
+++ src/main/java/com/goit/module4/UberShop.java
@@ -7,17 +7,29 @@ public class UberShop {
     public static void main(String[] args) {
         UberShop shop = new UberShop();
 
+        // Завдання 1
         //Should be 100 jup. 23.5 jup. 400 jup. - one price per line
         /*float[] prices = new float[] {100f, 23.5f, 400f};
-        shop.printPrices(prices);*/
+        shop.printPrices(prices);
+        */
 
+        // Завдання 2
         //Should be [200, 2250]
-        float[] prices = new float[]{100f, 1500f};
+        /*float[] prices = new float[]{100f, 1500f};
         shop.multiplyPrices(prices);
         System.out.println(Arrays.toString(prices));
-    }
+         */
 
-    /*public void printPrices(float[] prices) {
+        // Завдання 3
+        //Should be [50, 1500]
+        //int[] prices = new int[]{100, 1500, 300, 50};
+        //int[] prices = new int[]{49, 49, 49, 49};
+        int[] prices = new int[]{};
+        int[] minMax = shop.findMinMaxPrices(prices);
+        System.out.println(Arrays.toString(minMax));
+    }
+    // Завдання 1
+       /*public void printPrices(float[] prices) {
         for (float price : prices) {
             System.out.println(price + " jup." );
         }
@@ -35,16 +47,44 @@ public class UberShop {
        for (int i=0;i<prices.length;++i){
             System.out.println(prices[i]+ " jup." );
         }*/
-    public void multiplyPrices(float[] prices) {
+
+    // Завдання 2
+    /*public void multiplyPrices(float[] prices) {
         for (int i = 0; i < prices.length; i++) {
-            //float newPrices;
             if (prices[i] < 1000f) {
-                //System.out.println(prices[i] * 2);
                 prices[i]=prices[i] * 2;
             } else {
-                //System.out.println(prices[i] * 1.5);
                 prices[i]=prices[i] * 1.5f;
             }
         }
+    }*/
+
+    // Завдання 3
+    public int[] findMinMaxPrices(int[] prices) {
+        Arrays.sort(prices);
+        int lenMass = prices.length;
+        /*if (prices.length >= 2) {
+            int[] minMax = {prices[0], prices[prices.length - 1]};
+            return minMax;
+        } else if (prices[0]==prices[prices.length - 1]) {
+            int[] minMax ={prices[0]};
+            return minMax;
+        } else if (prices.length == 1) {
+            int[] minMax ={prices[0]};
+            return minMax;
+        } else {
+            int[] minMax = new int[0];
+            return minMax;
+        }*/
+        if (lenMass == 0) {
+            return new int[0];
+        } else if (prices[0] == prices[lenMass - 1]) {
+           return new int[]{prices[0]};
+        } else if (lenMass >= 2) {
+           return new int[]{prices[0], prices[lenMass - 1]};
+        } else if (lenMass == 1) {
+            return new int[]{prices[0]};
+        } else
+            return new int[0];
     }
 }
