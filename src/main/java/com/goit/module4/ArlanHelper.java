6a0f90f8c7a9c6c67701a8a0efdde36736b90a76
i.shurda <9594900@gmail.com>
2022-11-29 21:52:38 +0200

    Module4: tasks 11 array

--- src/main/java/com/goit/module4/ArlanHelper.java
+++ src/main/java/com/goit/module4/ArlanHelper.java
@@ -6,10 +6,20 @@ public class ArlanHelper {
     //Test output
     public static void main(String[] args) {
         ArlanHelper helper = new ArlanHelper();
+
+        //Завдання 1
         //Should be:
         //**
         //**
         System.out.println(helper.drawQuad(2));
+
+
+        //Завдання 2
+        //Should be:
+        //XX
+        //XX
+        //XX
+        System.out.println(helper.drawRect(2, 3, 'X'));
     }
     public String drawQuad(int n){
         int line = n;
@@ -31,5 +41,21 @@ public class ArlanHelper {
 //            System.out.println( Arrays.toString(strings));
 //        }
         return "";
+    }
+
+    public String drawRect(int width, int height, char c){
+        String resultC = "";
+        int i = 0;
+        int j = 0;
+        while (i<height) {
+            while (j<width){
+                resultC += c;
+                j++;
+            }
+            resultC +="\n";
+            i++;
+            j=0;
+        }
+        return resultC;
     }
 }
