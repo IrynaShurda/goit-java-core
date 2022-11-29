08fd0eb783422c054d3ff551c15010231d64ccbd
i.shurda <9594900@gmail.com>
2022-11-29 22:15:16 +0200

    Module4: tasks 12 array

--- src/main/java/com/goit/module4/ArlanHelper.java
+++ src/main/java/com/goit/module4/ArlanHelper.java
@@ -7,33 +7,39 @@ public class ArlanHelper {
     public static void main(String[] args) {
         ArlanHelper helper = new ArlanHelper();
 
-        //Завдання 1
+        //Завдання 10
         //Should be:
         //**
         //**
         System.out.println(helper.drawQuad(2));
 
 
-        //Завдання 2
+        //Завдання 11
         //Should be:
         //XX
         //XX
         //XX
         System.out.println(helper.drawRect(2, 3, 'X'));
+
+        //Завдання 12
+        //Should be:
+        //*#*#*
+        System.out.println(helper.drawLine(5));
     }
-    public String drawQuad(int n){
+
+    public String drawQuad(int n) {
         int line = n;
-        int column=n;
+        int column = n;
         int i = 0;
         int j = 0;
-        while (i<line) {
+        while (i < line) {
             while (j < column) {
                 //result +="*";
                 System.out.print("*");
                 j++;
             }
             i++;
-            j=0;
+            j = 0;
             //result+="\n";
             System.out.println();
         }
@@ -43,19 +49,34 @@ public class ArlanHelper {
         return "";
     }
 
-    public String drawRect(int width, int height, char c){
+    public String drawRect(int width, int height, char c) {
         String resultC = "";
         int i = 0;
         int j = 0;
-        while (i<height) {
-            while (j<width){
+        while (i < height) {
+            while (j < width) {
                 resultC += c;
                 j++;
             }
-            resultC +="\n";
+            resultC += "\n";
             i++;
-            j=0;
+            j = 0;
         }
         return resultC;
+    }
+    public String drawLine(int length){
+        String resultLine="";
+        int line = 0;
+        while (line<length ){
+            resultLine +="*";
+            line++;
+            while (line<length ){
+                resultLine +="#";
+                line++;
+                break;
+            }
+            resultLine +="";
+        }
+        return resultLine;
     }
 }
