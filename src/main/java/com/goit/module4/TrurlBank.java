6a01d95981dbbf4c65619daa7fb9e90becb1d045
i.shurda <9594900@gmail.com>
2022-12-01 11:54:20 +0200

    Module4: tasks 16 array

--- src/main/java/com/goit/module4/TrurlBank.java
+++ src/main/java/com/goit/module4/TrurlBank.java
@@ -6,15 +6,30 @@ public class TrurlBank {
 
         //Should be 14 - 1 + 4 + 9
         System.out.println(bank.sumQuads(3));
+
+        //Should be 16 - 5 + 4 + 7
+        System.out.println(bank.countSumOfDigits(547));
     }
 
     public int sumQuads(int n) {
-            int number = 1;
-            int sum=0;
-            while (number<=n){
-                sum +=Math.pow(number, 2);
-                number++;
-            }
-                return sum;
+        int number = 1;
+        int sum = 0;
+        while (number <= n) {
+            sum += Math.pow(number, 2);
+            number++;
+        }
+        return sum;
+    }
+
+    public int countSumOfDigits(int number) {
+        String stringNumber = Integer.toString(number);
+        int counter = 0;
+        int sum = 0;
+        while (counter < stringNumber.length()) {
+            sum += Integer.parseInt(Character.toString(stringNumber.charAt(counter)));
+            counter++;
+        }
+        return sum;
     }
 }
+
