a9c1f34d11c68dbfaca372a4918a529cf0e428e7
i.shurda <9594900@gmail.com>
2022-12-03 20:13:57 +0200

    Module5: tasks 3

--- src/main/java/com/goit/module5/SpaceShip.java
+++ src/main/java/com/goit/module5/SpaceShip.java
@@ -8,14 +8,22 @@ public class SpaceShip {
     }
 
     public void setName(String name) {
-        this.name = name;
+        if (name.isBlank()|| name.length()>100){
+            return;
+        }
+            this.name = name;
     }
 
     //Test output
     public static void main(String[] args) {
         SpaceShip ship = new SpaceShip();
-        System.out.println(ship.getName()); //Should be null
+        //System.out.println(ship.getName()); //Should be null
         ship.setName("Walker");
         System.out.println(ship.getName()); //Should be Walker
+        ship.setName("");
+        System.out.println(ship.getName()); //Should be Walker, empty value ignored
+
+        ship.setName("Voyager ".repeat(100));
+        System.out.println(ship.getName()); //Should be Walker, too long value ignored
     }
 }
