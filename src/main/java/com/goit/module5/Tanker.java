4ea90f118a40cf9d3d0db48b1d3cfc5364e771ab
i.shurda <9594900@gmail.com>
2022-12-06 13:58:11 +0200

    Module5: tasks 10

---
+++ src/main/java/com/goit/module5/Tanker.java
@@ -0,0 +1,37 @@
+package com.goit.module5;
+
+public class Tanker {
+    private String serialNumber;
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+    protected String getFuelType(){
+        return "A500";
+    }
+    public void refuel(int amount){
+        System.out.println("Add "+ amount+" of "+ getFuelType()+", tanker serial number is "+getSerialNumber());
+    }
+
+    public static void main(String[] args) {
+        Tanker tanker=new Tanker();
+        tanker.setSerialNumber("SN504030");
+        tanker.refuel(300);
+    }
+}
+class XFuelTanker extends Tanker{
+    @Override
+    protected String getFuelType() {
+        return "XFuel";
+    }
+}
+class MX200Tanker extends Tanker{
+    @Override
+    protected String getFuelType() {
+        return "MX200";
+    }
+}
