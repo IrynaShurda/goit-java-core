7f246790a11836682c3a6262741cedf3c44e5f05
i.shurda <9594900@gmail.com>
2022-12-08 00:35:48 +0200

    Module5: tasks 14

---
+++ src/main/java/com/goit/module5/Copyright.java
@@ -0,0 +1,11 @@
+package com.goit.module5;
+
+final class Copyright {
+
+    public static void main(String[] args) {
+        new Copyright().print();
+    }
+    public void print(){
+        System.out.println("SpaceWarriors Inc.");
+    }
+}
