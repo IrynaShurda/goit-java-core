3ff1a1f345490add49caa53e9222cc5c04d5eecd
i.shurda <9594900@gmail.com>
2022-12-08 22:05:26 +0200

    Module5: tasks 18

--- src/main/java/com/goit/module5/SpaceportMessages.java
+++ src/main/java/com/goit/module5/SpaceportMessages.java
@@ -3,9 +3,7 @@ package com.goit.module5;
 public class SpaceportMessages {
 
     public static void main(String[] args) {
-        String result = SpaceportMessages.generateLandingMessage("SunFlower", "EarthKeeper", "20:40");
-        System.out.println(result);
-
+        System.out.println(SpaceportMessages.generateLandingMessage("SunFlower", "EarthKeeper", "20:40"));
     }
     public static String generateLandingMessage(String shipName, String spaceport, String time){
         return "Spaceship "+ shipName+" landing at "+ spaceport+", time is " + time;
