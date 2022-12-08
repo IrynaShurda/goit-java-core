88f99b56c8038e919a5b87d88f4748cb9085871f
i.shurda <9594900@gmail.com>
2022-12-08 22:53:37 +0200

    Module5: tasks 20

--- src/main/java/com/goit/module5/PointTest.java
+++ src/main/java/com/goit/module5/PointTest.java
@@ -1,5 +1,7 @@
 package com.goit.module5;
 
+import java.util.Objects;
+
 public class PointTest {
     public static void main(String[] args) {
         Point p1 = new Point();
@@ -12,10 +14,32 @@ public class PointTest {
         p2.setY(1);
         p2.setZ(1);
 
+
+        Point p3 = new Point();
+        p3.setX(2);
+        p3.setY(2);
+        p3.setZ(3);
+
         System.out.println(p1.equals(p2)); //Will be false
+        System.out.println(p1.equals(p3)); //Should be false
     }
 }
 class Point{
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y && z == point.z;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, z);
+    }
+
+
     private int x;
     private int y;
     private int z;
