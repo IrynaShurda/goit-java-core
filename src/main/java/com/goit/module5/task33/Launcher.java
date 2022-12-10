5145b68ac7a8ce99ca7c2103d23974eba8129e42
i.shurda <9594900@gmail.com>
2022-12-10 22:51:14 +0200

    Module5: tasks 33

---
+++ src/main/java/com/goit/module5/task33/Launcher.java
@@ -0,0 +1,13 @@
+package com.goit.module5.task33;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Spaceport spaceport = new Spaceport();
+        spaceport.launch();
+    }
+}
+class Spaceport{
+    public void launch(){
+
+    }
+}
