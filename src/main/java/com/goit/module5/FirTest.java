df7156d00cd283c69ad11edc942410eaf0364758
i.shurda <9594900@gmail.com>
2022-12-10 16:26:32 +0200

    Module5: tasks 28

--- src/main/java/com/goit/module5/FirTest.java
+++ src/main/java/com/goit/module5/FirTest.java
@@ -7,15 +7,18 @@ public class FirTest {
 
     public static void main(String[] args) {
 //        FirNum firNum = new FirNum();
-        FirNum firNum = new FirNumSum();
+//        FirNum firNum = new FirNumSum();
+        FirNum firNum = new FirNumFactorial();
         FirTest firTest = new FirTest();
 
         //Should be 10
-        //System.out.println(firTest.test(firNum, 10));
+//        System.out.println(firTest.test(firNum, 10));
 
         //Should be 6
-        System.out.println(firTest.test(firNum, 3));
+//        System.out.println(firTest.test(firNum, 3));
 
+        //Should be 120
+        System.out.println(firTest.test(firNum, 5));
     }
 }
 
@@ -36,5 +39,22 @@ class FirNumSum extends FirNum {
             number++;
         }
         return sum;
+    }
+}
+class FirNumFactorial extends FirNum{
+    @Override
+    public int calc(int n) {
+        int number = 1;
+        int fucNum = 1;
+
+         if (n==0){
+             return fucNum;
+         }else
+
+        while (number <= n) {
+            fucNum *= number;
+            number++;
+        }
+        return fucNum;
     }
 }
