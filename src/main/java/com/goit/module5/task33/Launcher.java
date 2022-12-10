75263be40aec68779a2a39c4a9f67b6649bc7108
i.shurda <9594900@gmail.com>
2022-12-10 23:06:04 +0200

    Module5: tasks 36

--- src/main/java/com/goit/module5/task33/Launcher.java
+++ src/main/java/com/goit/module5/task33/Launcher.java
@@ -9,9 +9,11 @@ public class Launcher {
 class Spaceport{
     private GasStation gasStation = new GasStation();
     private ServiceTower serviceTower = new ServiceTower();
+    private CompressorStation compressorStation =new CompressorStation();
     public void launch(){
         gasStation.refuel();
         serviceTower.service();
+        compressorStation.compress();
 
     }
 }
@@ -25,5 +27,11 @@ class GasStation{
 class ServiceTower{
     public void service(){
         System.out.println("Service done!");
+    }
+}
+
+class CompressorStation{
+    public void compress(){
+        System.out.println("Compress done!");
     }
 }
