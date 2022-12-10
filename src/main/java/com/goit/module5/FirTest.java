cc3dc918581bc9e23500ede17de56cd71f4b5fc7
i.shurda <9594900@gmail.com>
2022-12-10 16:15:22 +0200

    Module5: tasks 27

--- src/main/java/com/goit/module5/FirTest.java
+++ src/main/java/com/goit/module5/FirTest.java
@@ -6,15 +6,35 @@ public class FirTest {
     }
 
     public static void main(String[] args) {
-        FirNum firNum = new FirNum();
+//        FirNum firNum = new FirNum();
+        FirNum firNum = new FirNumSum();
         FirTest firTest = new FirTest();
 
         //Should be 10
-        System.out.println(firTest.test(firNum, 10));
+        //System.out.println(firTest.test(firNum, 10));
+
+        //Should be 6
+        System.out.println(firTest.test(firNum, 3));
+
     }
 }
-class FirNum{
-    public int calc(int n){
+
+class FirNum {
+    public int calc(int n) {
         return n;
+    }
+}
+
+class FirNumSum extends FirNum {
+    @Override
+    public int calc(int n) {
+        //return super.calc(n);
+        int number = 1;
+        int sum = 0;
+        while (number <= n) {
+            sum += number;
+            number++;
+        }
+        return sum;
     }
 }
