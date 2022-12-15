40dad3b5820851360994ec4be152ff4d6b0a57d1
i.shurda <9594900@gmail.com>
2022-12-15 19:25:11 +0200

    Module6: tasks 17

--- src/main/java/com/goit/module6/LevelTest.java
+++ src/main/java/com/goit/module6/LevelTest.java
@@ -3,17 +3,46 @@ package com.goit.module6;
 public class LevelTest {
     public static void main(String[] args) {
         //Quarke level, name is Test
-        System.out.println(new Level("Test"));
+//        System.out.println(new Level("Test"));
+        Level.Point p1 = new Level.Point(3, 5);
+        Level.Point p2 = new Level.Point(10, 100);
+        Level.Point p3 = new Level.Point(50, 40);
+
+        Level.Point[] points = {p1, p2, p3};
+
+        //Quarke level, name is Test, point count is 3
+        System.out.println(new Level("Test", points));
     }
 }
 class Level{
-    String nameLevel;
-    public Level(String nameLevel) {
+    private String nameLevel;
+    private int point;
+    public Level(String nameLevel, Point[] points) {
         this.nameLevel=nameLevel;
+        this.point= points.length;
     }
 
     @Override
     public String toString() {
-        return "Quarke level, name is "+nameLevel;
+        return "Quarke level, name is "+nameLevel+", point count is "+point;
+    }
+    static class Point{
+        int x;
+        int y;
+        private Point[] points;
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+        public Point(int x, int y){
+            this.x=x;
+            this.y=y;
+
+        }
     }
 }
+
