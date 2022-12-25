d8dacb983944b2d2d2691975d3bff1109c80736b
i.shurda <9594900@gmail.com>
2022-12-25 18:58:47 +0200

    Module7: tasks 4

---
+++ src/main/java/com/goit/module7/PhraseTest.java
@@ -0,0 +1,37 @@
+package com.goit.module7;
+
+import java.util.Arrays;
+
+public class PhraseTest {
+    public static void main(String[] args) {
+        String[] words = {"alpha", "beta", "gamma"};
+
+        Phrase phrase = new Phrase(words);
+
+        //alpha beta gamma
+        System.out.println(phrase);
+
+        words[0] = "zero";
+
+        //alpha beta gamma
+        System.out.println(phrase);
+    }
+}
+
+class Phrase {
+    private final String[] words;
+
+
+    public Phrase(String[] words) {
+        this.words = Arrays.copyOf(words, words.length);
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        for (String singleWord : words) {
+            result += singleWord + " ";
+        }
+        return result;
+    }
+}
