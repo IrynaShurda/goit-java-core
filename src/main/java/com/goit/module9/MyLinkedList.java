24a424544b608355c4eeeda51b096915d4e31a28
i.shurda <9594900@gmail.com>
2023-01-24 13:13:40 +0200

    Module9: MyLinkedList

---
+++ src/main/java/com/goit/module9/MyLinkedList.java
@@ -0,0 +1,109 @@
+package com.goit.module9;
+
+import java.util.LinkedList;
+import java.util.StringJoiner;
+
+public class MyLinkedList<T> {
+    private int size = 0;
+
+    private Node<T> first;
+    private Node<T> last;
+
+    public void add(T value) {
+        final Node<T> l = last;
+        Node<T> newNode = new Node<>(l, value, null);
+        last = newNode;
+        if (size == 0) {
+            first = newNode;
+        } else {
+            l.next = newNode;
+        }
+        size++;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException(index);
+        }
+        Node<T> node = first;
+        int counter = 0;
+        while (counter != index) {
+            node = node.next;
+            counter++;
+        }
+        return (T) node.item;
+    }
+
+    public void clear() {
+        Node<T> node = first;
+        while (node != null) {
+            Node<T> x = node;
+            x.item = null;
+            x.next = null;
+            x.prev = null;
+            node = node.next;
+        }
+        first = null;
+        last = null;
+        size = 0;
+    }
+
+    public T remove(int index) {
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException(index);
+        }
+        Node<T> node = first;
+        int counter = 0;
+        while (counter != index) {
+            node = node.next;
+            counter++;
+        }
+        Node<T> x = node;
+        Node<T> prev = node.prev;
+        Node<T> next = node.next;
+
+        prev.next = next;
+        next.prev = prev;
+
+        size--;
+
+        return (T) node.item;
+    }
+
+    @Override
+    public String toString() {
+        if (first == null) {
+            return "[]";
+        }
+        Node<T> node = first.next;
+        String collection = node.prev.item.toString();
+        while (node != null) {
+            collection += ", " + node.item;
+            node = node.next;
+        }
+        return collection;
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "item=" + item +
+                    '}';
+        }
+    }
+}
--- src/main/java/com/goit/module9/TestCollections.java
+++ src/main/java/com/goit/module9/TestCollections.java
@@ -1,43 +1,94 @@
 package com.goit.module9;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class TestCollections {
 
     public static void main(String[] args) {
 
-
-        List<Object> list = new ArrayList<>();
+        List<Object> list = new LinkedList<>();
         MyArrayList<Integer> myArrayList = new MyArrayList();
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList();
         for (int i = 0; i < 12; i++) {
             list.add(i);
             myArrayList.add(i);
+            myLinkedList.add(i);
         }
 
+        System.out.println("============================");
+//        System.out.println("ArrayList");
+        System.out.println("LinkedList");
+        System.out.println();
+        System.out.println("ADD:");
         System.out.println(list.toString());
+        System.out.println();
+        System.out.println("SIZE:");
         System.out.println("size " + list.size());
+        System.out.println();
+        System.out.println("GET:");
         System.out.println("read index value " + list.get(4));
-//        System.out.println(list.get(13));
+//        System.out.println(list.get(13));  // for test exception
+        System.out.println();
+        System.out.println("REMOVE:");
         System.out.println("remove " + list.remove(4));
         System.out.println("after remove " + list);
         System.out.println("size after remove " + list.size());
+        System.out.println();
+        System.out.println("CLEAN:");
         list.clear();
         System.out.println("size " + list.size());
         System.out.println(list);
 
-        System.out.println("===========================");
-
-//        myArrayList.add("re");
+//   MyArrayList
+/*
+        System.out.println("============================");
+        System.out.println("MyArrayList");
+        System.out.println();
+        System.out.println("ADD:");
         System.out.println(myArrayList);
+        System.out.println();
+        System.out.println("SIZE:");
         System.out.println("size " + myArrayList.size());
+        System.out.println();
+        System.out.println("GET:");
         System.out.println("read index value " + myArrayList.get(4));
-//        System.out.println(myArrayList.get(13));
+//        System.out.println(myArrayList.get(13)); // for test exception
+        System.out.println();
+        System.out.println("REMOVE:");
         System.out.println("remove " + myArrayList.remove(4));
         System.out.println("after remove " + myArrayList);
         System.out.println("size after remove  " + myArrayList.size());
+        System.out.println();
+        System.out.println("CLEAR:");
         myArrayList.clear();
         System.out.println("size " + myArrayList.size());
         System.out.println(myArrayList);
+
+ */
+//   MyLinkedList
+        System.out.println("============================");
+        System.out.println("MyLinkedList");
+        System.out.println();
+        System.out.println("ADD:");
+        System.out.println("[" + myLinkedList + "]");
+        System.out.println();
+        System.out.println("SIZE:");
+        System.out.println("size " + myLinkedList.size());
+        System.out.println();
+        System.out.println("GET:");
+        System.out.println("read index value " + myLinkedList.get(4));
+//        System.out.println(myLinkedList.get(13));  // for test exception
+        System.out.println();
+        System.out.println("REMOVE:");
+        System.out.println("remove " + myLinkedList.remove(4));
+        System.out.println("after remove " + "[" + myLinkedList + "]");
+        System.out.println("size after remove  " + myLinkedList.size());
+        System.out.println();
+        System.out.println("CLEAR:");
+        myLinkedList.clear();
+        System.out.println("size " + myLinkedList.size());
+        System.out.println(myLinkedList);
     }
 }
