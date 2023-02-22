20a79d9454362b7006b66b32350752f119192a65
i.shurda <9594900@gmail.com>
2023-02-22 15:55:16 +0200

    Module12: tack2

--- src/main/java/com/goit/module12/task1/ConvertToFizzBuzz.java
+++ src/main/java/com/goit/module12/task1/ConvertToFizzBuzz.java
@@ -1,62 +1,65 @@
 package com.goit.module12.task1;
 
-import java.util.StringJoiner;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ConvertToFizzBuzz {
 
-    public static ConcurrentLinkedQueue queue = new ConcurrentLinkedQueue();
+    public static ConcurrentLinkedQueue<String> queue = new ConcurrentLinkedQueue();
     public static int n = 15;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        ExecutorService service = Executors.newFixedThreadPool(2);
-
-        service.submit(() -> {
-            while (true) {
-                StringJoiner joiner = new StringJoiner(", ");
-                if (queue.size() > 0) {
-//                    StringJoiner result = joiner.add(queue.poll().toString());
-                    System.out.print( queue.poll()+ ", ");
-//                    System.out.println(result);
-
-                }
-            }
-        });
+        ExecutorService service = Executors.newFixedThreadPool(4);
 
         for (int i = 1; i <= n; i++) {
             final int counter = i;
-
-            service.submit(() -> {
-                if (counter % 3 == 0 && counter % 5 != 0) {
-                    queue.offer("fizz");
-                }
-            });
-            service.submit(() -> {
-                if (counter % 5 == 0 && counter % 3 != 0) {
-                    queue.offer("buzz");
-                }
-            });
-            service.submit(() -> {
-                if (counter % 5 == 0 && counter % 3 == 0) {
-
-                    queue.offer("fizzbuzz");
-                }
-            });
-            service.submit(() -> {
-                number(counter);
-            });
+            threadA(service, counter);
+            threadB(service, counter);
+            threadC(service, counter);
+            threadD(service, counter);
         }
-        service.shutdown();
 
+        Thread.sleep(1000);
+        service.shutdownNow();
+        Object result = String.join(", ", queue);
+        System.out.println(result);
+    }
 
+    private static void threadA(ExecutorService service, int counter) {
+        service.submit(() -> {
+            if (counter % 3 == 0 && counter % 5 != 0) {
+                queue.offer("fizz");
+            }
+        });
+    }
+
+    private static void threadB(ExecutorService service, int counter) {
+        service.submit(() -> {
+            if (counter % 5 == 0 && counter % 3 != 0) {
+                queue.offer("buzz");
+            }
+        });
+    }
+
+    private static void threadC(ExecutorService service, int counter) {
+        service.submit(() -> {
+            if (counter % 5 == 0 && counter % 3 == 0) {
+                queue.offer("fizzbuzz");
+            }
+        });
+    }
+
+    private static void threadD(ExecutorService service, int counter) {
+        service.submit(() -> {
+            number(counter);
+        });
     }
 
     private static void number(int counter) {
         if (counter % 5 != 0 && counter % 3 != 0) {
-            queue.offer(counter);
+            queue.offer(String.valueOf(counter));
         }
     }
 }
