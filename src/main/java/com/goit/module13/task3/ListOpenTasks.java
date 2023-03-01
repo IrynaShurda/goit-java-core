1f263766947f285bc84d7fb13e78268c581ccfc3
i.shurda <9594900@gmail.com>
2023-03-01 13:09:51 +0200

    module13 task3

---
+++ src/main/java/com/goit/module13/task3/ListOpenTasks.java
@@ -0,0 +1,41 @@
+package com.goit.module13.task3;
+
+import com.goit.module13.task1.HttpMethods;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ListOpenTasks {
+
+    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
+        String uri = "https://jsonplaceholder.typicode.com/users";
+        String userID = "1";
+
+        HttpResponse<String> httpResponse = HttpMethods.getHttpRequest(uri + "/" + userID + "/todos");
+
+        List<UserTask> userTasks = new Gson().fromJson(httpResponse.body(), new TypeToken<ArrayList<UserTask>>() {
+        }.getType());
+
+        List<UserTask> openTasks = findNotCompletedTask(userTasks);
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        String toJson = gson.toJson(openTasks);
+
+        System.out.println("open tasks for userId = " + userID + " " + toJson);
+
+    }
+
+    private static List<UserTask> findNotCompletedTask(List<UserTask> userTasks) {
+        return userTasks.stream()
+                .filter(i -> i.isCompleted() == false)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/goit/module13/task3/UserTask.java
@@ -0,0 +1,41 @@
+package com.goit.module13.task3;
+
+public class UserTask {
+    private final int userId;
+    private final int id;
+    private final String title;
+    boolean completed;
+
+    public UserTask(int userId, int id, String title, boolean completed) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.completed = completed;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    @Override
+    public String toString() {
+        return "UserTask{" +
+                "userId=" + userId +
+                ", id=" + id +
+                ", title='" + title + '\'' +
+                ", completed=" + completed +
+                '}';
+    }
+}
